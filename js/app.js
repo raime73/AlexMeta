cb18fe765910535715a20e77891e1e3b89011dd0
raime73 <117109220+raime73@users.noreply.github.com>
2022-11-20 03:55:10 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,58 +3,58 @@ const coffees = [
   {
     name: "Ventas",
     image: "images/coffee1.jpg"
-    link: "ventas.html"
+   //  link: "ventas.html"
   },
   {
     name: "Gastos",
     image: "images/coffee2.jpg"
-    link: "gastos.html"
+  //  link: "gastos.html"
   },
   {
     name: "Reportes",
     image: "images/coffee3.jpg"
-    link: "reportes.html"
+    // link: "reportes.html"
   },
   {
     name: "Clientes",
     image: "images/coffee4.jpg"
-    link: "clientes.html"
+    // link: "clientes.html"
   },
   {
     name: "Aprende",
     image: "images/coffee6.jpg"
-    link: "aprende.html"
+    // link: "aprende.html"
   },
   {
     name: "Apoyo",
     image: "images/coffee7.jpg"
-    link: "apoyo.html"
+    // link: "apoyo.html"
   },
   {
     name: "Objetivos",
     image: "images/coffee5.jpg"
-    link: "objetivos.html"
+   //  link: "objetivos.html"
   },
   {
     name: "Mi Motor",
     image: "images/coffee8.jpg"
-    link: "motiva.html"
+   //  link: "motiva.html"
   },
   {
     name: "Info",
     image: "images/coffee9.jpg"
-    link: "about.html"
+   //  link: "about.html"
   }
 ];
 const showCoffees = () => {
   let output = "";
   coffees.forEach(
-    ({ name, image, link }) =>
+    ({ name, image}) =>
       (output += `
               <div class="card">
                 <img class="card--avatar" src=${image} />
                 <h1 class="card--title">${name}</h1>
-                <a class="card--link" href=${link}>IR</a>
+                <a class="card--link" href=${name}>IR</a>
               </div>
               `)
   );
