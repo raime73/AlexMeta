219144c492fa29949322d0172b1580398b85e22b
raime73 <117109220+raime73@users.noreply.github.com>
2022-11-20 01:17:17 -0600

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -8,20 +8,25 @@ const assets = [
   "/images/gastos.jpg",
   "/images/reportes.jpg",
   "/images/clientes.jpg",
+  "/images/aprende.jpg",
+  "/images/apoyo.jpg",
+  "/images/lista.jpg",
+  "/images/motiva.jpg",
+  "/images/acerca.jpg"
 ];
 
-// self.addEventListener("install", installEvent => {
-//  installEvent.waitUntil(
-//    caches.open(staticAlexMeta).then(cache => {
-//      cache.addAll(assets);
-//    })
-//  );
-//});
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDevCoffee).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
 
-//self.addEventListener("fetch", fetchEvent => {
-//  fetchEvent.respondWith(
-//   caches.match(fetchEvent.request).then(res => {
-//      return res || fetch(fetchEvent.request);
-//    })
-//  );
-//});
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
